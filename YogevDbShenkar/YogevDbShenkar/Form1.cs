a16cce9038b421a194179ca4cc8be00cfb36c370
Yogev911 <Yogev911@gmail.com>
2016-11-27 20:22:25 +0200

    filling up the tables

--- YogevDbShenkar/YogevDbShenkar/Form1.cs
+++ YogevDbShenkar/YogevDbShenkar/Form1.cs
@@ -27,8 +27,7 @@ namespace YogevDbShenkar
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (DBobj.OpenConnection() == true)
-            {
+            
                 string Drop1 = "DROP TABLE rooms";
                 string Drop2 = "DROP TABLE lecturers";
                 string Drop3 = "DROP TABLE courses";
@@ -38,17 +37,15 @@ namespace YogevDbShenkar
                 DBobj.DropTable(Drop2);
                 DBobj.DropTable(Drop3);
                 DBobj.DropTable(Drop4);
-
-
-            }
+                
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string Table1 = "CREATE TABLE IF NOT EXISTS rooms(room_number int, building varchar(255), floor int,PRIMARY KEY(room_number))";
-            string Table2 = "CREATE TABLE IF NOT EXISTS lecturers(id int, first_name varchar(255), last_name varchar(255),phone_number varchar(255),address varchar(255),PRIMARY KEY(id))";
-            string Table3 = "CREATE TABLE IF NOT EXISTS courses(course_number int, name varchar(255), year int,semester int,hours int, PRIMARY KEY(course_number))";
-            string Table4 = "CREATE TABLE IF NOT EXISTS schedule(course_number int, id int, room_number int,day int,hour int,PRIMARY KEY(course_number,id,room_number,day,hour))";
+            string Table1 = "CREATE TABLE IF NOT EXISTS rooms(room_number varchar(255), building varchar(255), floor varchar(255),PRIMARY KEY(room_number))";
+            string Table2 = "CREATE TABLE IF NOT EXISTS lecturers(id varchar(255), first_name varchar(255), last_name varchar(255),phone_number varchar(255),address varchar(255),PRIMARY KEY(id))";
+            string Table3 = "CREATE TABLE IF NOT EXISTS courses(course_number varchar(255), name varchar(255), year varchar(255),semester varchar(255),hours varchar(255), PRIMARY KEY(course_number))";
+            string Table4 = "CREATE TABLE IF NOT EXISTS schedule(course_number varchar(255), id varchar(255), room_number varchar(255),day varchar(255),hour varchar(255),PRIMARY KEY(course_number,id,room_number,day,hour))";
             DBobj.AddTable(Table1);
             DBobj.AddTable(Table2);
             DBobj.AddTable(Table3);
@@ -57,7 +54,7 @@ namespace YogevDbShenkar
 
         private void button4_Click(object sender, EventArgs e)
         {
-            int i = 0
+            int i = 0;
 
             string[] ARoom_number = new string[10]      { "247"     , "246"     , "204"     , "123"     , "435"     , "62"      , "345"     , "835"     , "124"     ,"2104"     };
             string[] ABuilding = new string[10]         { "Fernik"  , "Fernik"  , "Mitchel" , "Fernik"  , "Mitchel" , "Fernik"  , "Mitchel" , "Mitchel" , "Fernik"  , "Mitchel" };
@@ -67,10 +64,10 @@ namespace YogevDbShenkar
                 DBobj.InsertInto(String.Format("INSERT INTO rooms (room_number,building,floor)VALUES ('{0}','{1}','{2}')", ARoom_number[i], ABuilding[i], AFloor[i]));
             }
 
-            string[] Bid = new string[10] { "247", "246", "204", "123", "435", "62", "345", "835", "124", "2104" };
-            string[] Bfirst_name = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Blast_name = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Bphone_number = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
+            string[] Bid = new string[10] { "301234546", "456214046", "304812345", "304345796", "35844046", "30483953", "33456046", "304846843", "975424046", "302467846" };
+            string[] Bfirst_name = new string[10] { "Sheran", "Gal", "Fredreg", "Eitan", "Carlos", "Eran", "Eli", "Eliel", "Maor", "Gili" };
+            string[] Blast_name = new string[10] { "Yeini", "Alberman", "Harush", "Tibi", "Garcia", "Zehvi", "Dasa", "Perez", "Buzaglo", "Vermot" };
+            string[] Bphone_number = new string[10] { "0501987434", "0528814634", "0508822344", "0508111234", "0528877734", "0589999834", "0508811395", "0508198734", "0529878834", "0545818834" };
             string[] Baddress = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
             for (i = 0; i < 10; i++)
             {
@@ -78,26 +75,43 @@ namespace YogevDbShenkar
             }
 
 
-            string[] Ccourse_number = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Cname = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Cyear = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Csemester = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Chours = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
+            string[] Ccourse_number = new string[10] { "22345", "22346", "22347", "22358", "18374", "22359", "33456", "33457", "33458", "33459" };
+            string[] Cname = new string[10] { "Database", "communication1", "communication2", "calculus1", "calculus2", "Algorithms", "data structures", "Physics1", "Physics2", "Unix" };
+            string[] Cyear = new string[10] { "A", "A", "B", "A", "B", "B", "A", "B", "C", "C" };
+            string[] Csemester = new string[10] { "A", "A", "A", "B", "B", "A", "B", "B", "B", "A" };
+            string[] Chours = new string[10] { "3", "3", "3", "4", "5", "3", "3", "5", "4", "3" };
             for (i = 0; i < 10; i++)
             {
                 DBobj.InsertInto(String.Format("INSERT INTO courses (course_number,name,year,semester,hours)VALUES ('{0}','{1}','{2}','{3}','{4}')", Ccourse_number[i], Cname[i], Cyear[i], Csemester[i], Chours[i]));
             }
 
 
-            string[] Dcourse_number = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Did = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Droom_number = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Dday = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
-            string[] Dhour = new string[10] { "247", "246", "204", "247", "246", "204", "247", "246", "204", "2104" };
+            string[] Dcourse_number = new string[10] { "22345", "22346", "22347", "22358", "18374", "22359", "33456", "33457", "33458", "33459" };
+            string[] Did = new string[10] { "301234546", "456214046", "304812345", "304345796", "35844046", "30483953", "33456046", "304846843", "975424046", "302467846" };
+            string[] Droom_number = new string[10] { "247", "246", "204", "123", "435", "62", "345", "835", "124", "2104" };
+            string[] Dday = new string[10] { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "saturday", "tuesday", "wednesday" };
+            string[] Dhour = new string[10] { "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00" };
             for (i = 0; i < 10; i++)
             {
                 DBobj.InsertInto(String.Format("INSERT INTO schedule (course_number,id,room_number,day,hour)VALUES ('{0}','{1}','{2}','{3}','{4}')", Dcourse_number[i], Did[i], Droom_number[i], Dday[i], Dhour[i]));
             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string Table1 = "CREATE TABLE IF NOT EXISTS lecturers(id int, first_name varchar(255), last_name varchar(255),phone_number varchar(255),address varchar(255),PRIMARY KEY(id))";
+            DBobj.AddTable(Table1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DBobj.InsertInto(String.Format("INSERT INTO lecturers (id, first_name, last_name,phone_number,address)VALUES ('{0}','{1}','{2}','{3}','{4}')", "fvfve", "fvfve", "fvfve", "fvfve", "fvfve"));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string Drop2 = "DROP TABLE lecturers";
+            DBobj.DropTable(Drop2);
         }
     }
 
